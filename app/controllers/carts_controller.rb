7f38b82226e441dbbd23677b1c766a8c986d9089
Karthik Nair <nairkarthik1997@gmail.com>
2016-01-11 13:17:13 -0500

    updated cart logic

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,32 @@
 class CartsController < ApplicationController
- def show
-   @cart = current_cart
+ def add
+  id = params[:id]
+  if session[:cart] then
+   cart = session[:cart]
+  else
+   session[:cart] = {}
+   cart = session[:cart]
+  end
+  if cart[id] then
+   cart[id] = cart[id] + 1
+  else
+   cart[id] = 1
+  end
+  redirect_to :action => :index
  end
-
- def add_to_cart
-   current_cart.add_product(params[:item_id])
-   redirect_to cart_path(current_cart.id)
+ 
+ 
+ def clearCart
+  session[:cart] = nil
+  redirect_to :action => :index
+ end
+ 
+ def index
+  if sessin[:cart] then
+   @cart = session[:cart]
+  else
+   @cart = {}
+  end
  end
 end
 
