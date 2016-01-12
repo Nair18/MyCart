cf1e47dc76a4aff73dc04e632677e5d136af3a78
Karthik Nair <nairkarthik1997@gmail.com>
2016-01-12 12:51:52 +0530

    updated the schema

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,21 @@ ActiveRecord::Schema.define(version: 20160111060843) do
     t.string   "payment_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+     t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "user_name"
+    t.string   "email"
+    t.integer  "phone"
+    t.text     "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.integer  "card_no"
+    t.integer  "cvc"
+    t.integer  "user_id"
+    t.integer  "order_id" 
+   
+   
   end
 
   create_table "products", force: true do |t|
